df73991b9ee81bf213072cfba0529788268e3f08
Gunas12 <gunesmmmdova3@gmail.com>
2025-03-04 15:51:31 +0400

    mision_vision_footer_overlay_video

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import VideoOverlay from "./pages/VideoOverlay";
 import "./App.css";
 const App = () => {
+  const [showVideo, setShowVideo] = useState(true);
   return (
     <div>
+      {showVideo && <VideoOverlay onClose={() => setShowVideo(false)} />}
       <Navbar />
       <Outlet />
       <Footer />
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 import "./footer.css";
+import LogoEn from "../assets/images/4simlogoen.png";
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <hr />
+      <div className="footer-content">
+        <div className="footer-logo">
+          <img src={LogoEn} alt="4SIM_logo" />
+        </div>
+        <p>&copy; 2025. All rights reserved.</p>
+        <p></p>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;

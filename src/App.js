3eb9d2aec8f7e6f676a3ddbb0d250ffab937d234
Narmin Murshudova <nrmin.mrdova.01@bk.ru>
2025-03-04 13:47:31 +0400

    app.css changes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-
+import "./App.css";
 const App = () => {
   return (
     <div>
       <Navbar />
       <Outlet />
-      
       <Footer />
     </div>
   );

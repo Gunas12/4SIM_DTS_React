5b59764537749e2c3347d8bf6849c4a6671fd5f6
Gunas12 <gunesmmmdova3@gmail.com>
2025-03-06 16:39:15 +0400

    Language

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import "./navbar.css";
 import logo from "../assets/images/4simlogoaz.svg";
 import { FiMenu, FiX } from "react-icons/fi";
-
+import { useTranslation } from "react-i18next";
 const menuReducer = (state, action) => {
   switch (action.type) {
     case "TOGGLE_MENU":
@@ -15,7 +15,11 @@ const menuReducer = (state, action) => {
 
 const Navbar = () => {
   const [state, dispatch] = useReducer(menuReducer, { isOpen: false });
+  const { i18n } = useTranslation();
 
+  const toggleLanguage = () => {
+    i18n.changeLanguage(i18n.language === "az" ? "en" : "az");
+  };
   return (
     <nav>
       <div className="nav-container">
@@ -47,13 +51,20 @@ const Navbar = () => {
             <li>
               <Link to="/successstories">Success Stories</Link>
             </li>
-            <li>FAQ</li>
-            <li>Contact us</li>
+            <li>
+              {" "}
+              <Link to="/">FAQ</Link>
+            </li>
+            <li>
+              <Link to="/">Contact us</Link>
+            </li>
             <div className="navbar-buttons">
               <Link to="/apply">
                 <button className="apply-button">Apply Now</button>
               </Link>
-              <button className="language-button">AZ</button>
+              <button className="language-button" onClick={toggleLanguage}>
+                {i18n.language === "az" ? "EN" : "AZ"}
+              </button>
             </div>
           </ul>
         </div>

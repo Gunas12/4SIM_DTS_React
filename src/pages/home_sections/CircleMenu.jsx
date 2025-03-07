7c9ff22b91dfe6d221df00dc41ea47104433ce5f
Gunas12 <gunesmmmdova3@gmail.com>
2025-03-07 17:30:09 +0400

    changedvideo

--- src/pages/home_sections/CircleMenu.jsx
+++ src/pages/home_sections/CircleMenu.jsx
@@ -1,10 +1,22 @@
 import React, { useState } from "react";
 import "../home_sections/circlemenu.css";
-import videoSource from "../../assets/videos/Your_paragraph_text.mp4";
+import videoSource from "../../assets/videos/button_video.mp4";
 
 // import ProgramScope from "./programscope";
 
 const VideoComponent = () => {
+  const scrollToSection1 = () => {
+    const section = document.getElementById("abouttt");
+    section.scrollIntoView({ behavior: "smooth" });
+  };
+  const scrollToSection2 = () => {
+    const section = document.getElementById("Scope");
+    section.scrollIntoView({ behavior: "smooth" });
+  };
+  const scrollToSection3 = () => {
+    const section = document.getElementById("Mission");
+    section.scrollIntoView({ behavior: "smooth" });
+  };
   return (
     <div className="video-containerr">
       <video autoPlay muted loop>
@@ -12,17 +24,17 @@ const VideoComponent = () => {
         Your browser does not support the video tag.
       </video>
       <div className="circle-menu">
-        <div className="circle">
+        <div className="circle" onClick={scrollToSection1}>
           <a href="#abouttt" className="circle-link">
-            Proqram haqda məlumat
+            Proqram haqqında məlumat
           </a>
         </div>
-        <div className="circle">
+        <div className="circle" onClick={scrollToSection2}>
           <a href="#Scope" className="circle-link">
             Proqramın məqsədi
           </a>
         </div>
-        <div className="circle">
+        <div className="circle" onClick={scrollToSection3}>
           <a href="#Mission" className="circle-link">
             Missiya və vizyon
           </a>

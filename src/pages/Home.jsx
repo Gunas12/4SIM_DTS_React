3ff6e055a2901b724c9662ae4f9ca414080e582c
Gunas12 <gunesmmmdova3@gmail.com>
2025-03-12 15:36:48 +0400

    dfs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import CircleMenu from "./home_sections/CircleMenu";
 import MissionVision from "./home_sections/MissionVision";
-import AboutProgram from './home_sections/aboutprogram';
+import AboutProgram from "./home_sections/aboutprogram";
 import ProgramScope from "./home_sections/ProgramScope";
 function Home() {
   return (
     <>
       <CircleMenu />
-      <AboutProgram />
-      <ProgramScope />
-      <MissionVision />
     </>
   );
 }

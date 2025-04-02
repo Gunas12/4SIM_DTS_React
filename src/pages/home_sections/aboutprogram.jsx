04b4cae669b8c68ebcbe35725f7a19b597ceb5fa
Narmin Murshudova <nrmin.mrdova.01@bk.ru>
2025-04-02 11:54:01 +0400

    design changes

--- src/pages/home_sections/aboutprogram.jsx
+++ src/pages/home_sections/aboutprogram.jsx
@@ -11,17 +11,17 @@ const AboutProgram = () => {
       <div className="about-first-div">
         <h2 className="about-program-title">{t('About Program')}</h2>
         <p>{t('The Digital Transformation Program is spearheaded by the Center for Analysis and Coordination of the Fourth Industrial Revolution(4SIM), which oversees the program\'s coordination and execution.')}</p>
-        <p style={{ color: "#3484e5" }}>
+        <p style={{ color: "white" }}>
           {t('The program is implemented under the supervision of the Ministry of Economy of Azerbaijan(MoE), serving as the project sponsor.')}
         </p>
         <p>
           {t('Aligned with the 2022-2026 Socio-Economic Development Strategy,')}
-          <b>{t('the program aims to empower Azerbaijani businesses by supporting their comprehensive digital transformation.')}</b>
+          <b className="about-bold-text">{t('the program aims to empower Azerbaijani businesses by supporting their comprehensive digital transformation.')}</b>
           {t('It provides funding, capability-building support, and expert guidance to accelerate digital adoption across industries.')}
         </p>
         <p>
           {t('Through structured assessments, strategic roadmaps, financial assistance, and ongoing mentorship,')}
-          <b>{t('The Digital Transformation Support Program equips businesses with the tools needed to thrive in the digital era.')}</b>
+          <b className="about-bold-text">{t('The Digital Transformation Support Program equips businesses with the tools needed to thrive in the digital era.')}</b>
         </p>
       </div>
       <div className="about-second-div">

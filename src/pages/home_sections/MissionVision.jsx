f3a27231065e10b16854fd0103fb8a336428927d
Gunas12 <gunesmmmdova3@gmail.com>
2025-05-06 15:30:31 +0400

    carousellogo

--- src/pages/home_sections/MissionVision.jsx
+++ src/pages/home_sections/MissionVision.jsx
@@ -5,10 +5,13 @@ import { useTranslation } from "react-i18next";
 import gifImage1 from "../../assets/images/eae48a63124aef9c73cac6fbe722dd28425bcc6b.gif";
 import gifImage2 from "../../assets/images/f11ce2855a2b392e1fe4ee18ef747ae52a83ed98.gif";
 import img1 from "../../assets/images/8f8d3461d7f75301d0b7bc1f1efcf852c5b9f28b.png";
-import img2 from "../../assets/images/img2.png";
+import img2 from "../../assets/images/img2.png"; /*
 import img3 from "../../assets/images/Group 11.svg";
 import img4 from "../../assets/images/azerfloat.svg";
-import img5 from "../../assets/images/Frame 56.svg";
+import img5 from "../../assets/images/Frame 56.svg";*/
+import img3 from "../../assets/images/Property 1=image 1.png";
+import img4 from "../../assets/images/Property 1=logoSecond 1.png";
+import img5 from "../../assets/images/Property 1=logo 2.png";
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
